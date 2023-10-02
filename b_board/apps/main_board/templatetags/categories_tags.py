d5061f09f7bc63e205ce3be0adca78370aff3ae9
Aleksej Savica <sova11@mail.ru>
2023-10-02 13:27:08 +0300

    восстановил django-debug-toolbar

--- b_board/apps/main_board/templatetags/categories_tags.py
+++ b_board/apps/main_board/templatetags/categories_tags.py
@@ -4,7 +4,7 @@ from apps.main_board.models import Category
 register = template.Library()
 
 
-@register.inclusion_tag('main_board/categories.html')
+@register.inclusion_tag('includes/sidebar.html')
 def all_categories():
     cats = Category.objects.all()
     return {"cats": cats}
